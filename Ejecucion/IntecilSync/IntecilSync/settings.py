22edafb7acac4826e94313851c0bafec17f5a4cc
Marcelo <marcelotroquian10@gmail.com>
2024-09-08 23:52:12 -0300

    Dashboard v.1

--- Ejecucion/IntecilSync/IntecilSync/settings.py
+++ Ejecucion/IntecilSync/IntecilSync/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'intec'
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Ruta al directorio de archivos estáticos
+STATIC_URL = '/static/'
+
+# Ruta al directorio donde se recopilan los archivos estáticos para producción
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Directorios adicionales para buscar archivos estáticos
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
