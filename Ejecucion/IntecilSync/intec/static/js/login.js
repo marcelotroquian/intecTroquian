0655c333a527e9d1d1c7b7a12e55294560f342ac
Marcelo <marcelotroquian10@gmail.com>
2024-09-10 00:40:06 -0300

    login v.0.2

---
+++ Ejecucion/IntecilSync/intec/static/js/login.js
@@ -0,0 +1,21 @@
+{/* <script>
+    document.addEventListener("DOMContentLoaded", function() {
+        var errorMessage = document.getElementById("error-message");
+        errorMessage.classList.add("show");
+    });
+</script>
+
+
+<script>
+  document.getElementById('keepSessionCheckbox').addEventListener('change', function() {
+    // Verificar si el checkbox está marcado
+    if (this.checked) {
+      // Si está marcado, establecer un indicador de sesión activa en localStorage
+      localStorage.setItem('sessionActive', 'true');
+      
+    } else {
+      // Si no está marcado, eliminar el indicador de sesión activa de localStorage
+      localStorage.removeItem('sessionActive');
+    }
+  });
+  </script> */}
