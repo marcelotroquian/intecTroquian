ff5b455867e1c001ec2dbdc163b0d4901e56281b
Marcelo <marcelotroquian10@gmail.com>
2024-09-17 00:26:12 -0300

    Login V.01

--- Ejecucion/IntecilSync/IntecilSync/settings.py
+++ Ejecucion/IntecilSync/IntecilSync/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fvxk=+3gao^0o4i#i+h4^asx7mv5*xra+8-$p1)^19dh+^f6m#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- Ejecucion/IntecilSync/intec/views.py
+++ Ejecucion/IntecilSync/intec/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout as auth_logout
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
-
+@login_required
 def dashboard(request):
     return render(request, 'dashboard.html')
 
+
+@login_required
+def inicio(request):
+    return render(request, "visa/inicio.html")
 
 def login_view(request):
     if request.method == 'POST':
@@ -28,9 +32,10 @@ def login_view(request):
 
 
 def logout_view(request):
-    logout(request)
-    return redirect('login')  # Redirige a la página de login después del logout
+    if request.method == 'POST':
+        auth_logout(request)
+        return redirect('login')
+    
+    # Puedes redirigir a la página de login o mostrar un mensaje en caso de método GET
+    return redirect('login')
 
-
-def inicio(request):
-    return render(request, "visa/inicio.html")
